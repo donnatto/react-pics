14139ce6a28be6e3a5a1ffbb3a1b0fa316e22342
Edwin Dominguez <edwindominguez@hotmail.com>
2020-02-01 18:16:34 -0500

    add search bar comoponent and semantic ui

---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class SearchBar extends Component {
+  render() {
+    return (
+      <div className="ui segment">
+        <form action="" className="ui form">
+          <div className="field">
+            <label htmlFor="">Image Search</label>
+            <input type="text" name="" id=""/>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
