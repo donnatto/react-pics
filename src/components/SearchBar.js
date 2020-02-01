818a49a87b5fa22285727f4231210c370c9d5066
Edwin Dominguez <edwindominguez@hotmail.com>
2020-02-01 18:33:41 -0500

    add state and event handler to search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  state = { term: '' };
+
   render() {
     return (
-      <div className="ui segment">
-        <form action="" className="ui form">
-          <div className="field">
-            <label htmlFor="">Image Search</label>
-            <input type="text" name="" id=""/>
+      <div className='ui segment'>
+        <form action='' className='ui form'>
+          <div className='field'>
+            <label htmlFor=''>Image Search</label>
+            <input
+              type='text'
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
-    )
+    );
   }
 }
